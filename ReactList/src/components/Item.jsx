b8be60685a453ee727a4056ad36734ab08daaef5
Shantanu Dutta <shantanudutta07@gmail.com>
2024-07-29 21:43:43 +0530

    event handling in react

--- ReactList/src/components/Item.jsx
+++ ReactList/src/components/Item.jsx
@@ -1,9 +1,18 @@
 import style from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
+  const buyButtonClicked = (foodItem) => {
+    console.log(`${foodItem} is added to cart`);
+  };
   return (
     <li className={`${style["myList"]}`}>
       <span className={style["mySpan"]}>{foodItem}</span>
+      <button
+        className={`${style.myButton} btn btn-secondary`}
+        onClick={() => buyButtonClicked(foodItem)}
+      >
+        Buy Now
+      </button>
     </li>
   );
 };
