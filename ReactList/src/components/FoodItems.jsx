b91aabad6cda65f2b3ab3c89e5fe65fbe7baf3b2
Shantanu Dutta <shantanudutta07@gmail.com>
2024-07-29 22:04:53 +0530

    passing function as props

--- ReactList/src/components/FoodItems.jsx
+++ ReactList/src/components/FoodItems.jsx
@@ -1,10 +1,17 @@
 import Item from "./Item";
 
 const FoodItems = ({ items }) => {
+  const buyButtonClicked = (foodItem) => {
+    console.log(`${foodItem} is added to cart`);
+  };
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <Item key={item} foodItem={item}></Item>
+        <Item
+          key={item}
+          foodItem={item}
+          buyButtonClicked={() => buyButtonClicked(item)}
+        ></Item>
       ))}
     </ul>
   );
--- ReactList/src/components/Item.jsx
+++ ReactList/src/components/Item.jsx
@@ -1,15 +1,12 @@
 import style from "./Item.module.css";
 
-const Item = ({ foodItem }) => {
-  const buyButtonClicked = (foodItem) => {
-    console.log(`${foodItem} is added to cart`);
-  };
+const Item = ({ foodItem, buyButtonClicked }) => {
   return (
     <li className={`${style["myList"]}`}>
       <span className={style["mySpan"]}>{foodItem}</span>
       <button
         className={`${style.myButton} btn btn-secondary`}
-        onClick={() => buyButtonClicked(foodItem)}
+        onClick={buyButtonClicked}
       >
         Buy Now
       </button>
