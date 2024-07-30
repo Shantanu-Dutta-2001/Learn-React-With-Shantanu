83e306e81c9272417ded1ca169bee7b0894ac6c2
Shantanu Dutta <shantanudutta07@gmail.com>
2024-07-30 21:23:45 +0530

    add new item in list using state management

--- ReactList/src/App.jsx
+++ ReactList/src/App.jsx
@@ -6,26 +6,37 @@ import FoodItemInput from "./components/FoodItemInput";
 import { useState } from "react";
 
 function App() {
-  let foodItems = ["Roti", "Salad", "Paneer", "Mushroom", "Vegetables"];
-
   // let enteredTextStateArr = useState("nothing");
   // let enteredTextVal = enteredTextStateArr[0];
   // let setEnteredTextState = enteredTextStateArr[1];
 
   // OR IT CAN BE DECLARED AS
 
-  let [enteredTextVal, setEnteredTextState] = useState("nothing");
+  let [enteredTextVal, setEnteredTextState] = useState();
+
+  let [foodItems, setFoodItems] = useState([
+    "Roti",
+    "Salad",
+    "Paneer",
+    "Mushroom",
+    "Vegetables",
+  ]);
 
   const onKeyDown = (event) => {
-    console.log(event.target.value);
-    setEnteredTextState(event.target.value);
+    if (event.key === "Enter" && event.target.value.length > 0) {
+      setEnteredTextState(event.target.value);
+      let newFoodItem = event.target.value;
+      event.target.value = null;
+      let newFoodList = [...foodItems, newFoodItem];
+      setFoodItems(newFoodList);
+    }
   };
   return (
     <>
       <ChildContainer>
         <h2>Healthy Foods</h2>
         <FoodItemInput handleKeyDown={onKeyDown}></FoodItemInput>
-        <p>Food item you entered is : {enteredTextVal}</p>
+        <p>Last Food Item you entered is : {enteredTextVal}</p>
         <ErrorMessage items={foodItems}></ErrorMessage>
         <FoodItems items={foodItems}></FoodItems>
       </ChildContainer>
