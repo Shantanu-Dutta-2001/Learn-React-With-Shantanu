ee03b1447436a1433dd1cd2f6ee02d6d7a85e86a
Shantanu Dutta <shantanudutta07@gmail.com>
2024-08-07 22:35:44 +0530

    separation of business logic from UI

--- TodoList_Form_useReducer/src/dataStore/todo-items-store.jsx
+++ TodoList_Form_useReducer/src/dataStore/todo-items-store.jsx
@@ -1,7 +1,61 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const todoListItemContext = createContext({
   item: [],
   addNewItem: () => {},
   deleteItem: () => {},
 });
+
+const todoItemReducer = (currentTodoItems, action) => {
+  let newTodoItem = currentTodoItems;
+  if (action.type === "New_Item") {
+    newTodoItem = [
+      ...currentTodoItems,
+      { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+    ];
+  } else if (action.type === "Delete_Item") {
+    newTodoItem = currentTodoItems.filter(
+      (item) => item.name !== action.payload.itemName
+    );
+  }
+  return newTodoItem;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [todoListItem, dispatchTodoItems] = useReducer(todoItemReducer, []);
+
+  const onNewItemAdd = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "New_Item",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+  };
+
+  const onDeleteItem = (todoItemName) => {
+    const deleteItemAction = {
+      type: "Delete_Item",
+      payload: {
+        itemName: todoItemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
+  };
+
+  return (
+    <todoListItemContext.Provider
+      value={{
+        item: todoListItem,
+        addNewItem: onNewItemAdd,
+        deleteItem: onDeleteItem,
+      }}
+    >
+      {children}
+    </todoListItemContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
