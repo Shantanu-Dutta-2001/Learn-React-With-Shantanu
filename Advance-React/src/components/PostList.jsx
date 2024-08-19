0818dcfce06cf2f29f8c22f1ad49726c2106299e
Shantanu Dutta <shantanudutta07@gmail.com>
2024-08-19 21:42:10 +0530

    implement loader for api call

--- Advance-React/src/components/PostList.jsx
+++ Advance-React/src/components/PostList.jsx
@@ -2,20 +2,25 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import WelcomeMessage from "./WelcomeMessage";
 import { PostList as PostListData } from "../store/post-list-store";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPost(data.posts);
+        setFetching(false);
       });
   }, []);
 
   return (
     <div>
-      {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {fetching && <LoadingSpinner></LoadingSpinner>}
+      {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
       {postList.map((item) => (
         <Post key={item.id} post={item}></Post>
       ))}
