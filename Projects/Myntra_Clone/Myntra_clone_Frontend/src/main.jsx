7a0b43e28d00a6b3992faacb083cdfd10d884c49
Shantanu Dutta <shantanudutta07@gmail.com>
2024-09-25 21:56:36 +0530

    initial redux store for the project

--- Projects/Myntra_Clone/Myntra_clone_Frontend/src/main.jsx
+++ Projects/Myntra_Clone/Myntra_clone_Frontend/src/main.jsx
@@ -1,27 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./routes/App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./routes/Home.jsx";
 import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
+import { Provider } from "react-redux";
+import myntraStore from "./store/index.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <Home /> /*, loader: postLoader */ },
+      { path: "/", element: <Home /> },
       {
         path: "/bag",
         element: <Bag />,
-        /*action: createPostAction,*/
       },
     ],
   },
 ]);
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
